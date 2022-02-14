e42f433875bbfc0ea6c0598b03b4dc51d8927b1b
woodwardem <woodwardem@g.crookcountyschools.org>
2022-02-14 08:51:52 -0800

    working on lab 8

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const { request, response } = require('express');
 const weather = require('./data/weather.json');
 const app = express();
+const axios = require('axios');
 app.use(cors());
 
 
@@ -16,16 +17,21 @@ app.get('/', (request, response) => {
     response.send('hello from home route')
 });
 
-app.get('/weather', (request, response) => {
+app.get('/weather', async (request, response) => {
     let lat = request.query.lat;
     let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;
 
-const city = weather.find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase());
+// const city = weather.find(cityObj => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase());
+const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7lat=${lat}&lon=${lon}`;
+const weatherData =  await axios.get(url);
+console.log(url);
+console.log(weatherData);
+console.log("lat", "lon", lat, lon);
 
 try{
-const weatherArray = city.data.map(day => new Forecast(day));
-response.status(200).send(weatherArray);
+console weatherArray = weatherData.data.data
+response.status(200).send(weatherData.data.data);
 
 
 } catch{error} {
