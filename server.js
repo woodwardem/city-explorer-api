994162d249b3dfc1a5f9fad80ea6cf7e5c9049a0
woodwardem <woodwardem@g.crookcountyschools.org>
2022-02-15 08:51:38 -0800

    going through back end of lab8

--- server.js
+++ server.js
@@ -41,10 +41,49 @@ response.status(200).send(weatherArray);
 }
 
 })
+app.get('/movies', async (request, response) => {
+    let searchQuery= request.query.searchQuery;
+    console.log(searchQuery);
+
+    const url= `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&query=${searchQuery}`
+    try{
+        const movieData = await axios.get(url);
+        console.log(movieData.data.results);
+        const movieData = movieData.data.results.map(movie => new Movie(day));
+        response.status(200).send(movieArray);
+        
+        
+        } catch{error} {
+            console.log(error);
+            response.status(500).send('movie not found');
+            response.send('movie not found');
+        }
+        
+
+})
+
+
+
+
 function Forecast(day) {
     this.day = day.valid_date
     this.description = day.weather.description
 }
+function Movie(movie) {
+    this.title = movie.title,
+    this.overview= movie.overview,
+    this.average_votes = movie.vote_average,
+    this.total_votes= movie.vote_count,
+  this.image_url = 'https://image.tmdb.org/t/p/w500' + movie.poster_path, 
+    this.popularity= movie.popularity,
+    this.released_on = movie.release_date
+        
+         
+        
+    
+}
+
+
 
 app.use('*', (request, response) => response.status(404).send('that end point does not exist'));
 
