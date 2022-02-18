63e3e82ab1f2cca5e56637dc72a40b65d11929f8
McCall Woodward <woodwardem@g.crookcountyschools.org>
2022-02-18 09:04:18 -0800

    fixed weather function

--- server.js
+++ server.js
@@ -16,26 +16,26 @@ app.get('/', (request, response) => {
     response.send('hello from home route')
 });
 
-app.get('/weather', async(request, response) => {
+app.get('/weather', async (request, response) => {
     let lat = request.query.lat;
     let lon = request.query.lon;
     let searchQuery = request.query.searchQuery;
 
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${lat}&lon=${lon}`;
+
+    try {
+        const weatherData = await axios.get(url);
+        console.log(weatherData.data.data)
+        const weatherArray = weatherData.data.data.map(day => new Forecast(day));
+        //const weatherData = city.data.map(day => new Forecast(day));
+        response.status(200).send(weatherArray);
 
 
-try{
-    const weatherData = await axios.get(url);
-    console.log(weatherData.data.data)
-    const weatherArray = weatherData.data.data.map(day => new Forecast(day));
-const weatherData = city.data.map(day => new Forecast(day));
-response.status(200).send(weatherArray);
-
-
-} catch{error} {
-    console.log(error);
-    response.status(500).send('city not found');
-    response.send('city not found');
-}
+    } catch { error } {
+        console.log(error);
+        response.status(500).send('city not found');
+        //response.send('city not found');
+    }
 
 })
 function Forecast(day) {
